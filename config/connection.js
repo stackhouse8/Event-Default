ea284dcccd3b0ed79a4ddb5b498ab2fb12afcf88
Masonb18 <bauer.mason01@gmail.com>
2023-06-14 18:40:20 -0500

    files/packages

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-<<<<<<< HEAD
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3001,
-  }
-);
-=======
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -28,6 +16,5 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
->>>>>>> 7303011d168cf4645be23456fd2a43b8f1ebefd2
 
 module.exports = sequelize;
