d785c2e76e4f815a8859b9b9e0e5cf29d4032df6
Masonb18 <bauer.mason01@gmail.com>
2023-06-21 16:23:58 -0500

    minor changes and working sign up

--- public/js/events.js
+++ public/js/events.js
@@ -2,9 +2,9 @@ const eventHandler = async (event) => {
     event.preventDefault();
 
     const eventName = document.querySelector("eventName")
-    const eventDate = document.querySelector("eventName")
-    const eventLocation = document.querySelector("eventName")
-    const eventDescription = document.querySelector("eventName")
+    const eventDate = document.querySelector("eventDate")
+    const eventLocation = document.querySelector("eventLocation")
+    const eventDescription = document.querySelector("eventDescription")
 
     if (loggedIn) {
         const render = await fetch('')
